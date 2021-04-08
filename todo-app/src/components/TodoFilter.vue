6814fd6a2baee282f33ab23c2ad3580560294f7b
kawasaki <k.kawasaki0820@gmail.com>
2021-04-08 14:01:29 +0900

    コンポーネントで分割したToDoアプリを作成

---
+++ todo-app/src/components/TodoFilter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ul>
+      <li><label><input name="filter" type="radio" value="すべて" v-model="showStatus">すべて</label></li>
+      <li><label><input name="filter" type="radio" value="作業中" v-model="showStatus">作業中</label></li>
+      <li><label><input name="filter" type="radio" value="完了" v-model="showStatus">完了</label></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoFilter",
+  data() {
+    return {
+      showStatus: 'すべて'
+    }
+  },
+  watch: {
+    showStatus() {
+      this.$emit('change-show-status', this.showStatus);
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  padding: 0;
+  display: flex;
+}
+
+li {
+  list-style: none;
+}
+</style>
